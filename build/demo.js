882324630ebff569f58e386220f77a5f7039399b
SivaBharat <sivabharthece@gmail.com>
2023-03-01 15:12:01 +0530

    task2Commit

--- build/demo.js
+++ build/demo.js
@@ -8,7 +8,7 @@ class Employee {
         this.empproject = empproject;
     }
     getempdetail() {
-        console.log("employee id=" + this.empid +" "+ "employee name:" + this.empname +" "+ "employee designation:" + this.empdesignation +" "+ "employee salary:" + this.empsalary +" "+ "employee project:" + this.empproject);
+        console.log("employee id=" + this.empid + "employee name:" + this.empname + "employee designation:" + this.empdesignation + "employee salary:" + this.empsalary + "employee project:" + this.empproject);
     }
 }
 const emp = new Employee(15, "Siva", "Developer", 20000, "angular");
